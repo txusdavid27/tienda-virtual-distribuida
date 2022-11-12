388a9f56283d85d12a40b42cfe880e827a8e8b38
txusdavid27 <jetraslavina@javeriana.edu.co>
2022-11-12 17:10:03 -0500

    Go Angie

--- Interfaz.java
+++
@@ -1,7 +0,0 @@
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface Interfaz extends Remote{
-    void consultar() throws RemoteException;
-    void adquirir(float id, float cantidad) throws RemoteException;
-}
